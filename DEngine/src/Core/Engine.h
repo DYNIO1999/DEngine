1fa18c6d5281e172e938aa126e626b3457c42bb7
DYNIO <dd7412506@gmail.com>
2022-05-14 14:01:13 +0200

    test

---
+++ DEngine/src/Core/Engine.h
@@ -0,0 +1,15 @@
+#ifndef _CORE_ENGINE_H_
+#define _CORE_ENGINE_H_
+
+namespace DEngine{
+class Engine
+{
+
+public:
+    Engine();
+    ~Engine();
+
+private:
+};
+}
+#endif
--- DEngine/src/Core/Window.h
+++ DEngine/src/Core/Window.h
@@ -1,1 +1,27 @@
-#include <iostream>
+#ifndef _CORE_WINDOW_H_
+#define _CORE_WINDOW_H_
+
+
+#include <memory>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "LogManager.h"
+
+
+namespace DEngine{
+
+
+class Window
+{
+public:
+    Window();
+    ~Window();
+
+    
+private:
+
+
+};
+}
+#endif
